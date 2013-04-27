83821ad49bdd931f582ee50f1f6e9a876e519c90
Ian Morrison <ian.morrison.a@gmail.com>
2013-04-27 15:26:52 -0700

    set up animations routes and create action

--- app/controllers/animations_controller.rb
+++ app/controllers/animations_controller.rb
@@ -1,5 +1,14 @@
 class AnimationsController < ApplicationController
   def index
   end
-  
+
+  def create
+    @animation = Animation.build(params[:animation])
+    
+    if @animation.save
+      render json: @animation
+    else
+      render json: @animation.errors.full_messages, status: 422
+    end
+  end
 end

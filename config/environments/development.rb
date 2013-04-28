72ca8edc304e998f2a987d7d5d86f9fbf338af8f
Ian Morrison <ian.morrison.a@gmail.com>
2013-04-27 22:45:24 -0700

    set up basic config for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Exquisitegif::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # added for divise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end

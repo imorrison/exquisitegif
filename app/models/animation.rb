70b2e7493796692efdebac3027079f4c1e036f73
Ian Morrison <ian.morrison.a@gmail.com>
2013-04-29 11:55:52 -0700

    provide helper methods for build_gif

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -5,21 +5,33 @@ class Animation < ActiveRecord::Base
 
 
   def build_gif
-    # causes in errror if run twice
-    dir = "#{Rails.root}/tmp/uploads/test10/"
-    
+    dir = dir_name 
     FileUtils.mkdir(dir)
 
-    self.frames.each do |frame|
+    build_frames(dir)
+    run_image_magick(dir)
+  end
 
-      File.open("#{dir}animation#{frame.id}.gif", 'wb') do|f|
+  private
+
+  def dir_name
+    "#{Rails.root}/tmp/uploads/#{SecureRandom.hex}/"
+  end
+
+  def build_frames(dir)
+    self.frames.each_with_index do |frame, i|
+
+      file_name = "#{i}".rjust(6, "0")
+
+      File.open("#{dir}frame#{file_name}.gif", 'wb') do|f|
         f.write(Base64.decode64(frame.data_url))
       end
     end
+  end
 
+  def run_image_magick(dir)
     image = MiniMagick::Image.new(dir)
-
-    image.run_command("convert -delay 20 -loop 0 #{dir}*.gif #{dir}test.gif")
+    image.run_command("convert -delay 20 -loop 0 #{dir}*.gif #{dir}animation.gif")
   end
 end
  
